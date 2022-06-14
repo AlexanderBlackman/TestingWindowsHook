768f032e762e0947474a31ae5677b8a232388150
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-14 12:14:36 +1000

    Add project files.

---
+++ TestingWindowsHook/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.ObjectModel;
+using WindowsHookEx;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TestingWindowsHook
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.ExtendsContentIntoTitleBar = true;
+            Subscribe();
+        }
+
+        ObservableCollection<string> clicksList { get; set; } = new ObservableCollection<String>();
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            clicksList.Add($"KeyPress: \t{e.KeyChar}");
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+
+            clicksList.Add($"MouseDown: \t{e.Button}; \t System Timestamp: \t{e.Timestamp}");
+
+            // uncommenting the following line will suppress the middle mouse button click
+            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+    }
+}
